29a7e032dc6d4e5843addf11cb9f2a29f1f361e9
LRoman303 <echeverryluis6@gmail.com>
2026-03-06 12:46:36 -0700

    Update linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -11,66 +11,77 @@ class EventLinkedList:
   def __init__(self):
   self.head = None
 
-def push(self):
-  newNode = Node(event)
-  newNode.next = self.head
-  self.head = newNode
+  def push(self, event):
+    newNode = Node(event)
+    newNode.next = self.head
+    self.head = newNode
 
 # Length of linked list
-def length(self):
-  size = 0 
-  temp = self.head 
-  while temp != None:
-    size += 1 
-    temp = temp.next 
-  return size
+  def length(self):
+    size = 0 
+    temp = self.head 
+    while temp != None:
+      size += 1 
+      temp = temp.next 
+    return size
  
 
 # Insert to linked list
-def insert(self, index, event):
-  if index == 0:
-    self.push(event)
-    return
-    
-  counter = 0
-  temp = self.head
-
-  newNode = Node(event)
-  length = self.length()
-  
-  if length <= index or index < 0:
-    raise ValueError("Index is out of bounds")
-  
-  else:
-    while counter != index -1:
-      counter += 1 
-      temp = temp.next
+  def insert(self, index, event):
+    if index == 0:
+      self.push(event)
+      return
       
-    newNode.next = temp.next
-    temp.next = newNode
-
-def search_by_id(self, target_id):
-  counter = 0 
-  temp = self.head
-  length = self.length()
+    counter = 0
+    temp = self.head
   
-  if length <= index or index < 0:
-    raise ValueError("Index is out of bounds")
-
-  else: 
-    while counter != index -1 :
-      counter +=1 
-      temp = temp.next
+    newNode = Node(event)
+    length_ll = self.length()
     
-    return temp.event
+    if length_ll < index or index < 0:
+      raise ValueError("Index is out of bounds")
+    
+    else:
+      while counter != index -1:
+        counter += 1 
+        temp = temp.next
+        
+      newNode.next = temp.next
+      temp.next = newNode
   
-
-def delete(self, target_id):
-  pass
-
-def list_all(self):
-  pass
-
+  def search_by_id(self, target_id):
+    temp = self.head 
+  
+    while temp != None:
+      if temp.event.id == target_id:
+        return temp.event
+      temp = temp.next
+    # If no target_id is found after traversing linked list return None
+    return None
+    
+  
+  def delete(self, target_id):
+    temp = self.head
+  
+    if temp.event.id == target_id:
+      self.head = self.head.next
+      return
+  
+    while temp.next != None:
+      if temp.next.event.id == target_id:
+        temp.next = temp.next.next
+      temp = temp.next
+  
+  def list_all(self):
+  
+    output = ""
+    temp = self.head
+  
+    while temp != None:
+      output += str(temp.event) + "->"
+      temp = temp.next
+    print(output)
+    
 
 
 ### Store events (call event-creator.py)
