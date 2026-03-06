ed309503204aaddbc276b6bc329a58d97bce7a3b
LRoman303 <echeverryluis6@gmail.com>
2026-03-06 14:13:45 -0700

    Update sample_array.py

--- src/sample_array.py
+++ src/sample_array.py
@@ -1,11 +1,11 @@
 from event_creator import Event
 # SIMPLE ARRAY OF CUSTOM MADE EVENTS TO USE FOR TESTING
 
-brunch = Event(0, "Brunch Buddies", "2026-03-06", "09:00", "C4C")
-lunch = Event(1, "Lunch & Learn", "2026-03-06", "11:30", "UMC")
-hhour = Event(2, "Bites & Beers", "2026-03-06", "16:30", "Avery")
-play = Event(3, "Play & Pals", "2026-03-06", "18:30", "Mary Rippon Outdoor Theatre")
-stars = Event(4, "Stars & Stares", "2026-03-06", "22:00", "Flagstaff")
+brunch = Event( "Brunch Buddies", "2026-03-06", "09:00", "C4C", id = 0)
+lunch = Event("Lunch & Learn", "2026-03-06", "11:30", "UMC", id = 1)
+hhour = Event("Bites & Beers", "2026-03-06", "16:30", "Avery", id = 2)
+play = Event("Play & Pals", "2026-03-06", "18:30", "Mary Rippon Outdoor Theatre", id = 3)
+stars = Event("Stars & Stares", "2026-03-06", "22:00", "Flagstaff", id = 4)
 
 friday = [brunch, lunch, hhour, play, stars]
 
