441668a5b75da7acbefb12306faf72334186fbf2
LRoman303 <echeverryluis6@gmail.com>
2026-03-06 17:52:44 -0700

    Update array.py

--- src/array.py
+++ src/array.py
@@ -2,9 +2,19 @@ import event_creator, sorting, searching
 from event_creator import Event
 
 ### ARRAY IMPLEMENTATION OF SCHEDULING SYSTERM
-class DyanamicArray(event):
+class DyanamicArray(object):
   def __init__(self):
+    # Length of array
     self.size = 0
+    self.capacity = 1
+    self.array = 
+
+  
+  def __len__(self):
+    """
+    Returns the length of the array
+    """
+    return self.size
 
 ### Store events (call event-creator.py)
 eventArray = []
