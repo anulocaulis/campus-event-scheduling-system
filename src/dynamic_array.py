68eb0a44c58b2242cf638a2f93918e7b5b59761e
LRoman303 <echeverryluis6@gmail.com>
2026-03-06 21:34:19 -0700

    Update dynamic_array.py

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -74,16 +74,16 @@ class DynamicArrayEvent():
   # Lists all events in dynamic array.
   def list_all(self):
     # Create empty list to display events
-    temp = []
+    eventArray = []
     # set counter to zero
     i = 0
     # Run while inside the array
     while i < self.size:
       # Add event into empty list
-      temp.append(self.array[i])
+      eventArray.append(self.array[i])
       i += 1
     # Return list. 
-    return temp
+    return eventArray
       
   
     
