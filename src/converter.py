3b0cc081d72419fd0b997b1e07e65fc48cf53d8b
Bockslunch <155188366+Bockslunch@users.noreply.github.com>
2026-03-07 13:24:59 -0700

    Added short explanation of converter function

--- src/converter.py
+++ src/converter.py
@@ -1,5 +1,13 @@
 # FUNCTION TO CONVERT LINKED LIST INTO AN ARRAY FOR EASIER SORTING
+
 def converter(linkedList):
+    """
+    CONVERTS LINKED LIST INTO AN ARRAY FOR COMPARATIVELY EASY SORTING
+    PARAMS:
+        linkedList - LINKED LIST OF EVENT OBJECTS
+    RETURNS:
+        res - ARRAY BUILT FROM LINKED LIST ELEMENTS
+    """
     res = []
     curr = linkedList.head
     while curr:
