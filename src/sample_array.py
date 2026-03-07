d2f0da63a3a09a3ef08c6299433b14db02946e30
LRoman303 <echeverryluis6@gmail.com>
2026-03-06 21:24:34 -0700

    Just moved Id to end to make sure the operations from LinkedList and DynamicArray work

--- src/sample_array.py
+++ src/sample_array.py
@@ -1,6 +1,5 @@
 from event_creator import Event
 # SIMPLE ARRAY OF CUSTOM MADE EVENTS TO USE FOR TESTING
-
 brunch = Event( "Brunch Buddies", "2026-03-06", "09:00", "C4C", id = 0)
 lunch = Event("Lunch & Learn", "2026-03-06", "11:30", "UMC", id = 1)
 hhour = Event("Bites & Beers", "2026-03-06", "16:30", "Avery", id = 2)
