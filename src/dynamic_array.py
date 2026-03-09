2d35c677e24e5685f6b44e4015975bce28411f02
LRoman303 <echeverryluis6@gmail.com>
2026-03-08 19:14:13 -0600

    Created get_at_Index method
    Need this method for better testing and more flexible use of Dynamic Array

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -11,6 +11,8 @@ resize: Time Complexity O(n) - Loop to traverse the dynamic array.
                                Resizing method depends on the length of the dynamic array.
 __len__: Time Complexity O(1) - Returns length of list at constant time,
                                 no matter how long dynamic array is.
+get_at_index: Time Complextiy O(1) - Returns event of list at given index,
+                                    no matter long dynamic array is.
 insert: Time Complexity O(n) - Loops to traverse the dynamic array to 
                                find targeted index to insert event.
 sort: Time Complexity O(nlogn) - average for mergeSort and quickSort
@@ -76,7 +78,24 @@ class DynamicArrayEvent:
       i += 1
     # Provides new array with old array values.
     self.array = new_array
-  
+  # Get at index method
+  def get_at_index(self, index):
+      """
+      Returns event at given index
+      PARAMS:
+          self - Array, where event is being returned from
+          index - Position where event is returned from
+      RETURNS:
+          event - Returns event at given index.
+      """
+       # Raise
+      if index > self.size or index < 0:
+          raise ValueError("This index is out of bounds.")
+      
+      # Return Event at given index
+      return self.array[index]
+
+
   # Append method
   def append(self, event):
     """
